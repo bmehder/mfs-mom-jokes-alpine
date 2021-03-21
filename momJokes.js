f52bbef215df6c15c660fb8bb167f2d7e33bd92f
bmehder <bmehder@gmail.com>
2021-03-20 19:06:55 -0500

    initial commit

---
+++ momJokes.js
@@ -0,0 +1,35 @@
+const jokes = [
+  { q: 'What did the baby corn say to the mama corn?', a: "Where's Popcorn?" },
+  {
+    q: 'Why is a computer so smart?',
+    a: 'Cause it listens to its motherboard.',
+  },
+  { q: "What do you call a mom who can't draw?", a: 'Tracy.' },
+  {
+    q: 'Why did the cookie cry?',
+    a: 'Because his mother was a wafer so long!',
+  },
+  {
+    q: 'What are the three quickest ways of spreading a rumour (or gossip)?',
+    a: 'The internet, telephone, tell your mom.',
+  },
+  { q: 'What do you call a small mom?', a: 'Minimum.' },
+  { q: 'What did the mama tomato say to the baby tomato?', a: 'Catch up!' },
+  { q: "Why don't mothers wear watches?", a: "There's a clock on the stove." },
+  { q: 'Why did the baby strawberry cry?', a: 'Because his mom was in a jam!' },
+  {
+    q: 'What did the baby Egyptian say when he got lost?',
+    a: 'I want my mummy.',
+  },
+  { q: 'What did the momma say to the foal?', a: 'Its pasture your bedtime.' },
+
+  { q: 'What book do moms like the most?', a: "Their husband's checkbook!" },
+  {
+    q: 'What did mommy spider say to baby spider?',
+    a: 'You spend too much time on the web.',
+  },
+  {
+    q: 'What did the mother broom say to the baby broom?',
+    a: "It's time to go to sweep!",
+  },
+];
